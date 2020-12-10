643636fe08415b17af2ade4a59a8773cdf1ebf9b
dword <sebastianlandw@gmail.com>
2020-12-10 23:14:32 +0000

    fix: replace get-package-name with depcheck-package-name

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import pushPlugins from '@dword-design/nuxt-push-plugins'
-import getPackageName from 'get-package-name'
+import packageName from 'depcheck-package-name'
 
 export default function (options = {}) {
-  this.addModule([getPackageName(require.resolve('nuxt-buefy')), options])
+  this.addModule([packageName`nuxt-buefy`, options])
   pushPlugins(this, require.resolve('./plugin'))
   if (options.css) {
     this.options.css.push(require.resolve('./style.scss'))

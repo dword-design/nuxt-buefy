ac2680d7a19cdabe93fd89500c06948d0a28c2e0
dword <info@dword-design.de>
2021-04-27 09:31:31 +0000

    fix: await addModule

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import packageName from 'depcheck-package-name'
 import nuxtPushPlugins from 'nuxt-push-plugins'
 
-export default function (options = {}) {
-  this.addModule([packageName`nuxt-buefy`, options])
+export default async function (options = {}) {
+  await this.addModule([packageName`nuxt-buefy`, options])
   nuxtPushPlugins(this, require.resolve('./plugin'))
   if (options.css) {
     this.options.css.push(require.resolve('./style.scss'))

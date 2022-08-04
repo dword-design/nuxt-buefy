2ba2728a7788aeac1c9638881630a5ad3a1f9ef3
Sebastian Landwehr <sebastian.landwehr@honestly.com>
2022-08-04 14:40:07 +0200

    fix: set link active class

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import nuxtPushPlugins from 'nuxt-push-plugins'
 
 export default function () {
+  this.options.router.linkActiveClass = 'is-active'
   nuxtPushPlugins(this, require.resolve('./plugin'))
 }

52add21801721d4839e34f4581e443d27dc2fc90
Sebastian Landwehr <sebastian.landwehr@honestly.com>
2022-08-04 14:34:57 +0200

    fix: self-implement nuxt-buefy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
-import packageName from 'depcheck-package-name'
 import nuxtPushPlugins from 'nuxt-push-plugins'
 
-export default async function (options = {}) {
-  await this.addModule([packageName`nuxt-buefy`, options])
+export default function () {
   nuxtPushPlugins(this, require.resolve('./plugin'))
-  if (options.css) {
-    this.options.css.push(require.resolve('./style.scss'))
-  }
 }
--- src/plugin.js
+++ src/plugin.js
@@ -1,4 +1,10 @@
 import AddonsPlugin from '@dword-design/buefy-addons'
+import SvgIcon from '@dword-design/buefy-svg-icon'
+import Buefy from 'buefy'
 import Vue from 'vue'
 
+Vue.use(Buefy, {
+  defaultIconComponent: SvgIcon,
+  defaultIconPack: null,
+})
 Vue.use(AddonsPlugin)

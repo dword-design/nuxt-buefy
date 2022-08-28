40bc0f05f48242478e73300437f1cb9cfab24232
Sebastian Landwehr <info@sebastianlandwehr.com>
2022-08-28 09:43:26 +0000

    fix: remove router link class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import nuxtPushPlugins from 'nuxt-push-plugins'
 
 export default function () {
-  this.options.router.linkActiveClass = 'is-active'
   nuxtPushPlugins(this, require.resolve('./plugin'))
 }

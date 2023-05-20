e0bb1ee2b88fa7d46d48b4ec30aa1f7c06a79487
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-05-20 16:31:19 +0000

    fix: foo

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,39 +1,38 @@
 import { endent } from '@dword-design/functions'
 import tester from '@dword-design/tester'
 import testerPluginPuppeteer from '@dword-design/tester-plugin-puppeteer'
-import fs from 'fs-extra'
-import { Builder, Nuxt } from 'nuxt'
+import { execaCommand } from 'execa'
+import nuxtDevReady from 'nuxt-dev-ready'
+import outputFiles from 'output-files'
+import kill from 'tree-kill-promise'
 import withLocalTmpDir from 'with-local-tmp-dir'
-
-import self from './index.js'
 
 export default tester(
   {
     works() {
       return withLocalTmpDir(async () => {
-        await fs.outputFile(
-          'pages/index.vue',
-          endent`
+        await outputFiles({
+          'nuxt.config.js': endent`
+            export default {
+              modules: ['../src/index.js'],
+            }
+          `,
+          'pages/index.vue': endent`
             <template>
               <b-button class="foo">foo</b-button>
             </template>
           `,
-        )
-
-        const nuxt = new Nuxt({
-          dev: false,
-          modules: [self],
         })
-        await new Builder(nuxt).build()
+
+        const nuxt = execaCommand('nuxt dev')
         try {
-          await nuxt.listen()
+          await nuxtDevReady()
           await this.page.goto('http://localhost:3000')
 
           const button = await this.page.waitForSelector('.foo')
           expect(await button.screenshot()).toMatchImageSnapshot(this)
         } finally {
-          await new Promise(resolve => setTimeout(resolve, 30000))
-          await nuxt.close()
+          await kill(nuxt.pid)
         }
       })
     },

e61990831964d3c2142cd2b999c783fbf3d3431c
Sebastian Landwehr <info@sebastianlandwehr.com>
2024-06-23 21:55:10 +0000

    feat: auto import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-import { addPlugin, createResolver } from '@nuxt/kit'
+import { addComponent, addPlugin, createResolver } from '@nuxt/kit'
+
+import components from './components.js'
 
 const resolver = createResolver(import.meta.url)
 
-export default () =>
+export default () => {
+  for (const component of components) {
+    const componentName = `B${component}`
+    addComponent({
+      export: componentName,
+      filePath: `buefy/dist/esm/${component.toLowerCase()}.js`,
+      name: componentName,
+    })
+  }
   addPlugin(resolver.resolve('./plugin.js'), { append: true })
+}

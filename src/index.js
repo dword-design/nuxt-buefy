7e35b263b64bb5cd80bdbf72f539334274f2b28c
Sebastian Landwehr <info@sebastianlandwehr.com>
2024-06-23 23:32:21 +0000

    fix: revert changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-import { addComponent, addPlugin, createResolver } from '@nuxt/kit'
-
-import components from './components.js'
+import { addPlugin, createResolver } from '@nuxt/kit'
 
 const resolver = createResolver(import.meta.url)
 
-export default () => {
-  for (const component of components) {
-    const componentName = `B${component}`
-    addComponent({
-      export: componentName,
-      filePath: `buefy/dist/esm/${component.toLowerCase()}.js`,
-      name: componentName,
-    })
-  }
-  addPlugin(resolver.resolve('./plugin.js'), { append: true })
-}
+export default () => addPlugin(resolver.resolve('./plugin.js'), { append: true })
--- src/plugin.js
+++ src/plugin.js
@@ -1,11 +1,11 @@
 import BuefyAddons from '@dword-design/buefy-addons'
 import SvgIcon from '@dword-design/buefy-svg-icon'
-import { ConfigProgrammatic } from 'buefy'
+import Buefy from 'buefy'
 
 import { defineNuxtPlugin } from '#imports'
 
 export default defineNuxtPlugin(nuxtApp => {
-  ConfigProgrammatic.setOptions({
+  nuxtApp.vueApp.use(Buefy, {
     defaultIconComponent: SvgIcon,
     defaultIconPack: undefined,
   })

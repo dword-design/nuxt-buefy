1851b6997c03579ac12b77f17832906c5f669ce3
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-07-26 20:48:09 +0200

    fix: test icons (#223)

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,4 +1,5 @@
 import { expect, test } from '@playwright/test';
+import packageName from 'depcheck-package-name';
 import endent from 'endent';
 import { execaCommand } from 'execa';
 import getPort from 'get-port';
@@ -76,6 +77,51 @@ test('works', async ({ page }, testInfo) => {
     const button = page.locator('.foo');
     await expect(button).toBeVisible();
     await expect(button).toHaveScreenshot();
+  } finally {
+    await kill(nuxt.pid);
+  }
+});
+
+test('icon', async ({ page }, testInfo) => {
+  const cwd = testInfo.outputPath();
+
+  await outputFiles(cwd, {
+    'nuxt.config.ts': endent`
+      import viteSvgLoader from '${packageName`vite-svg-loader`}';
+
+      export default {
+        css: ['../../src/style.scss'],
+        modules: ['../../src'],
+        vite: {
+          plugins: [viteSvgLoader()],
+        }
+      }
+    `,
+    'pages/index.vue': endent`
+      <template>
+        <b-icon class="foo":icon="CheckIcon" />
+      </template>
+
+      <script setup lang="ts">
+      import CheckIcon from '${packageName`@mdi/svg`}/svg/check.svg?component';
+      </script>
+    `,
+  });
+
+  const port = await getPort();
+
+  const nuxt = execaCommand('nuxt dev', {
+    cwd,
+    env: { NODE_ENV: '', PORT: String(port) },
+    reject: false,
+  });
+
+  try {
+    await nuxtDevReady(port);
+    await page.goto(`http://localhost:${port}`);
+    const icon = page.locator('.foo');
+    await expect(icon).toBeVisible();
+    await expect(icon).toHaveScreenshot();
   } finally {
     await kill(nuxt.pid);
   }
--- src/plugin.ts
+++ src/plugin.ts
@@ -1,14 +1,11 @@
-import BuefyAddons from '@dword-design/buefy-addons';
 import SvgIcon from '@dword-design/buefy-svg-icon';
 import { ConfigProgrammatic } from 'buefy';
 
 import { defineNuxtPlugin } from '#imports';
 
-export default defineNuxtPlugin(({ vueApp }) => {
+export default defineNuxtPlugin(() =>
   ConfigProgrammatic.setOptions({
     defaultIconComponent: SvgIcon,
     defaultIconPack: undefined,
-  });
-
-  vueApp.use(BuefyAddons);
-});
+  }),
+);

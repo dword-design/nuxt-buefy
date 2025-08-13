d7c1cfcd439623771916afe84cad24d4ac1c56c4
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-08-13 03:56:41 +0200

    fix: add other programmatics

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,11 +1,29 @@
 import SvgIcon from '@dword-design/buefy-svg-icon';
-import { ConfigProgrammatic } from 'buefy';
+import {
+  ConfigProgrammatic,
+  DialogProgrammatic,
+  LoadingProgrammatic,
+  ModalProgrammatic,
+  NotificationProgrammatic,
+  SnackbarProgrammatic,
+  ToastProgrammatic,
+} from 'buefy';
 
 import { defineNuxtPlugin } from '#imports';
 
-export default defineNuxtPlugin(() =>
+export default defineNuxtPlugin(({ vueApp }) => {
   ConfigProgrammatic.setOptions({
     defaultIconComponent: SvgIcon,
     defaultIconPack: undefined,
-  }),
-);
+  });
+
+  vueApp.config.globalProperties.$buefy = {
+    config: ConfigProgrammatic,
+    dialog: new DialogProgrammatic(vueApp),
+    loading: new LoadingProgrammatic(vueApp),
+    modal: new ModalProgrammatic(vueApp),
+    notification: new NotificationProgrammatic(vueApp),
+    snackbar: new SnackbarProgrammatic(vueApp),
+    toast: new ToastProgrammatic(vueApp),
+  };
+});
